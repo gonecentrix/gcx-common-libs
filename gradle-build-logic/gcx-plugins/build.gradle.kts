003e0532688897314e0cdd09b44f2cb6ad1eaf82
Bruno Pessanha <bruno.pessanha-de-carvalho@grandcentrix.net>
2022-04-08 13:19:53 +0200

    Centralise version declarations inside gradle-platform

--- gradle-build-logic/gcx-plugins/build.gradle.kts
+++ gradle-build-logic/gcx-plugins/build.gradle.kts
@@ -1,3 +1,7 @@
+import java.io.File
+import java.io.FileInputStream
+import java.util.Properties
+
 group = "net.grandcentrix.plugin"
 
 plugins {
@@ -5,27 +9,20 @@ plugins {
     `maven-publish`
 }
 
-dependencies {
-    // implementation(platform("net.grandcentrix.component:gradle-platform"))
-    // implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin")
-    // implementation("org.jetbrains.kotlin.plugin.spring:org.jetbrains.kotlin.plugin.spring.gradle.plugin")
-    // implementation("org.jetbrains.kotlin.plugin.jpa:org.jetbrains.kotlin.plugin.jpa.gradle.plugin")
-    // implementation("org.springframework.boot:org.springframework.boot.gradle.plugin")
-    // implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin")
-    // implementation("org.owasp.dependencycheck:org.owasp.dependencycheck.gradle.plugin")
-    // implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin")
-    // implementation("com.github.ben-manes.versions:com.github.ben-manes.versions.gradle.plugin")
-    // implementation("io.gitlab.arturbosch.detekt:io.gitlab.arturbosch.detekt.gradle.plugin")
+val platformProperties = Properties().apply {
+    load(FileInputStream(File("gradle-platform", "gradle.properties")))
+}
 
-    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.6.10")
-    implementation("org.jetbrains.kotlin.plugin.spring:org.jetbrains.kotlin.plugin.spring.gradle.plugin:1.6.10")
-    implementation("org.jetbrains.kotlin.plugin.jpa:org.jetbrains.kotlin.plugin.jpa.gradle.plugin:1.6.10")
-    implementation("org.springframework.boot:org.springframework.boot.gradle.plugin:2.6.5")
-    implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:10.2.1")
-    implementation("org.owasp.dependencycheck:org.owasp.dependencycheck.gradle.plugin:7.0.0")
-    implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.0.11.RELEASE")
-    implementation("com.github.ben-manes.versions:com.github.ben-manes.versions.gradle.plugin:0.42.0")
-    implementation("io.gitlab.arturbosch.detekt:io.gitlab.arturbosch.detekt.gradle.plugin:1.19.0")
+dependencies {
+    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:${platformProperties["kotlinVersion"]}")
+    implementation("org.jetbrains.kotlin.plugin.spring:org.jetbrains.kotlin.plugin.spring.gradle.plugin:${platformProperties["kotlinVersion"]}")
+    implementation("org.jetbrains.kotlin.plugin.jpa:org.jetbrains.kotlin.plugin.jpa.gradle.plugin:${platformProperties["kotlinVersion"]}")
+    implementation("org.springframework.boot:org.springframework.boot.gradle.plugin:${platformProperties["springBootVersion"]}")
+    implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:${platformProperties["ktLintVersion"]}")
+    implementation("org.owasp.dependencycheck:org.owasp.dependencycheck.gradle.plugin:${platformProperties["owaspDependencyCheckVersion"]}")
+    implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:${platformProperties["springDependencyManagementVersion"]}")
+    implementation("com.github.ben-manes.versions:com.github.ben-manes.versions.gradle.plugin:${platformProperties["gradleVersionsVersion"]}")
+    implementation("io.gitlab.arturbosch.detekt:io.gitlab.arturbosch.detekt.gradle.plugin:${platformProperties["detektVersion"]}")
 }
 
 publishing {

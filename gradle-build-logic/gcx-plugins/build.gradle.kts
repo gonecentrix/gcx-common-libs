15d319753c0bf712772a5c0164514839810907dc
Bruno Pessanha <bruno.pessanha-de-carvalho@grandcentrix.net>
2022-04-13 14:40:13 +0200

    Add symlink to gradle.properties

--- gradle-build-logic/gcx-plugins/build.gradle.kts
+++ gradle-build-logic/gcx-plugins/build.gradle.kts
@@ -1,7 +1,3 @@
-import java.io.File
-import java.io.FileInputStream
-import java.util.Properties
-
 group = "net.grandcentrix.plugin"
 
 plugins {
@@ -9,20 +5,16 @@ plugins {
     `maven-publish`
 }
 
-val platformProperties = Properties().apply {
-    load(FileInputStream(File("gradle-platform", "gradle.properties")))
-}
-
 dependencies {
-    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:${platformProperties["kotlinVersion"]}")
-    implementation("org.jetbrains.kotlin.plugin.spring:org.jetbrains.kotlin.plugin.spring.gradle.plugin:${platformProperties["kotlinVersion"]}")
-    implementation("org.jetbrains.kotlin.plugin.jpa:org.jetbrains.kotlin.plugin.jpa.gradle.plugin:${platformProperties["kotlinVersion"]}")
-    implementation("org.springframework.boot:org.springframework.boot.gradle.plugin:${platformProperties["springBootVersion"]}")
-    implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:${platformProperties["ktLintVersion"]}")
-    implementation("org.owasp.dependencycheck:org.owasp.dependencycheck.gradle.plugin:${platformProperties["owaspDependencyCheckVersion"]}")
-    implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:${platformProperties["springDependencyManagementVersion"]}")
-    implementation("com.github.ben-manes.versions:com.github.ben-manes.versions.gradle.plugin:${platformProperties["gradleVersionsVersion"]}")
-    implementation("io.gitlab.arturbosch.detekt:io.gitlab.arturbosch.detekt.gradle.plugin:${platformProperties["detektVersion"]}")
+    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:${properties["kotlinVersion"]}")
+    implementation("org.jetbrains.kotlin.plugin.spring:org.jetbrains.kotlin.plugin.spring.gradle.plugin:${properties["kotlinVersion"]}")
+    implementation("org.jetbrains.kotlin.plugin.jpa:org.jetbrains.kotlin.plugin.jpa.gradle.plugin:${properties["kotlinVersion"]}")
+    implementation("org.springframework.boot:org.springframework.boot.gradle.plugin:${properties["springBootVersion"]}")
+    implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:${properties["ktLintVersion"]}")
+    implementation("org.owasp.dependencycheck:org.owasp.dependencycheck.gradle.plugin:${properties["owaspDependencyCheckVersion"]}")
+    implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:${properties["springDependencyManagementVersion"]}")
+    implementation("com.github.ben-manes.versions:com.github.ben-manes.versions.gradle.plugin:${properties["gradleVersionsVersion"]}")
+    implementation("io.gitlab.arturbosch.detekt:io.gitlab.arturbosch.detekt.gradle.plugin:${properties["detektVersion"]}")
 }
 
 publishing {
--- libs/base/src/main/kotlin/net/grandcentrix/component/base/autoconfigure/BaseAutoConfiguration.kt
+++ libs/base/src/main/kotlin/net/grandcentrix/component/base/autoconfigure/BaseAutoConfiguration.kt
@@ -6,6 +6,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
 @Configuration(proxyBeanMethods = false)
-@Import(JpaAuditingConfiguration::class,)
+@Import(JpaAuditingConfiguration::class)
 @AutoConfigureAfter(WebMvcAutoConfiguration::class)
 class BaseAutoConfiguration

6036da9a287d72c3a9a58fd26ec467954e347942
Bruno Pessanha <bruno.pessanha-de-carvalho@grandcentrix.net>
2022-08-09 12:54:37 +0200

    Add documentation to CustomRepositoryContext and RepositoryWithExclusiveLock (#20)

--- libs/base-entity/src/main/kotlin/net/grandcentrix/component/base/repository/CustomRepositoryContext.kt
+++ libs/base-entity/src/main/kotlin/net/grandcentrix/component/base/repository/CustomRepositoryContext.kt
@@ -4,14 +4,32 @@ import org.springframework.stereotype.Component
 import javax.persistence.EntityManager
 import javax.persistence.Query
 
+/**
+ * This class provides a function to programmatically set a lock timeout for PostgreSQL databases.
+ * In addition, it provides an after query hook that is called from RepositoryWithExclusiveLock, after executing
+ * a select query.
+ *
+ * @see net.grandcentrix.component.base.repository.RepositoryWithExclusiveLock
+ */
 @Component
 class CustomRepositoryContext {
+
+    /**
+     * Used in integration tests, this function is used keep the CPU busy and raise timeout exceptions,
+     * in order to ensure that the lock timeout has been set correctly.
+     */
     fun afterQueryHook() {
         // do nothing
     }
 
+    /**
+     * Sets the lock timeout
+     *
+     * @param entityManager The entity manager
+     * @param timeoutDurationInMs the duration of the timeout in milliseconds
+     */
     fun setLockTimeout(entityManager: EntityManager, timeoutDurationInMs: Long) {
-        val query: Query = entityManager.createNativeQuery("set local lock_timeout = $timeoutDurationInMs")
+        val query: Query = entityManager.createNativeQuery("SET LOCAL LOCK_TIMEOUT = $timeoutDurationInMs")
         query.executeUpdate()
     }
 }
--- libs/base-entity/src/main/kotlin/net/grandcentrix/component/base/repository/RepositoryWithExclusiveLock.kt
+++ libs/base-entity/src/main/kotlin/net/grandcentrix/component/base/repository/RepositoryWithExclusiveLock.kt
@@ -8,6 +8,42 @@ import javax.persistence.EntityManager
 import javax.persistence.LockModeType
 import kotlin.reflect.KClass
 
+/**
+ * This class facilitates selecting entities for update (i.e. using pessimistic locking/write).
+ * It selects any record by its id applying a lock timeout based on your configuration properties.
+ *
+ * The use case for selecting an entity for update and acquiring a lock on that specific database record is when your
+ * system tries to concurrently select the same entity and then tries to update it.
+ *
+ * By default, JPA repositories use Optimistic locking strategies, which means that if two processes load
+ * the same entity to memory they will both have the same `version`.
+ *
+ * However, one of these processes will be able to persist the changes to that entity before the other,
+ * increasing the `version` of that entity. When the second process tries to persist its changes, the second
+ * process will fail due to an `OptmisticLockException`.
+ *
+ * This repository enables the synchronisation of these processes, the first process tries to load the entity
+ * into memory (via a select for update), PostgreSQL will set a lock on that record, so the second process will
+ * be blocked (and will not load that entity into memory) until the first process has committed its transaction.
+ *
+ * To use this component, just add it to your service dependencies, and call it specifying your entity as a return type:
+ * @Service
+ * class MyService(private val repositoryWithExclusiveLock: RepositoryWithExclusiveLock) {
+ *     fun getEntityById(id: UUID): Entity = repositoryWithExclusiveLock
+ *         .findAndObtainExclusiveLockOnItById(id) ?: throw NotFoundException("Entity with id $id was not found.")
+ * }
+ *
+ * You can also just use the annotation @Lock(LockModeType.PESSIMISTIC_WRITE), however you cannot set the lock timeout
+ * which defaults to NO_WAIT
+ *
+ * @see net.grandcentrix.component.base.config.DataSourceProperties
+ * @see https://www.postgresql.org/docs/current/runtime-config-client.html#GUC-LOCK-TIMEOUT
+ *
+ * @param entityManager The entity manager injected automatically
+ * @param customRepositoryContext The custom repository context which sets the lock timeout
+ * @param dataSourceProperties Data source properties containing the lock timeout duration
+ *
+ */
 @Component
 class RepositoryWithExclusiveLock(
     private val entityManager: EntityManager,
@@ -19,7 +55,7 @@ class RepositoryWithExclusiveLock(
         customRepositoryContext.setLockTimeout(entityManager, dataSourceProperties.lockTimeoutMs)
 
         val query = entityManager.createQuery(
-            "SELECT c FROM " + ofClass.simpleName + " c where c.id = :id"
+            "SELECT c FROM " + ofClass.simpleName + " c WHERE c.id = :id"
         ).apply {
             setParameter("id", id)
             lockMode = LockModeType.PESSIMISTIC_WRITE

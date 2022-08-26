04998b0efc80d89a3f862f6fc0a399c1e057b6b7
Bruno Pessanha <bruno.pessanha-de-carvalho@grandcentrix.net>
2022-08-26 12:46:04 +0200

    fix: include RepositoryWithExclusiveLock into auto configuration (#21)

--- libs/base-entity/src/main/kotlin/net/grandcentrix/component/base/entity/autoconfigure/BaseAutoConfiguration.kt
+++ libs/base-entity/src/main/kotlin/net/grandcentrix/component/base/entity/autoconfigure/BaseAutoConfiguration.kt
@@ -1,11 +1,20 @@
 package net.grandcentrix.component.base.entity.autoconfigure
 
+import net.grandcentrix.component.base.config.DataSourceProperties
+import net.grandcentrix.component.base.repository.CustomRepositoryContext
+import net.grandcentrix.component.base.repository.RepositoryWithExclusiveLock
+import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration
-import org.springframework.context.annotation.Configuration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Import
 
-@Configuration(proxyBeanMethods = false)
-@Import(JpaAuditingConfiguration::class)
+@AutoConfiguration
 @AutoConfigureAfter(WebMvcAutoConfiguration::class)
+@Import(
+    JpaAuditingConfiguration::class,
+    RepositoryWithExclusiveLock::class,
+    CustomRepositoryContext::class,
+)
+@EnableConfigurationProperties(DataSourceProperties::class)
 class BaseAutoConfiguration

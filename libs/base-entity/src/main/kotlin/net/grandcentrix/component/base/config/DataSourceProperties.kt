cd1b887eec4f2d17fb9d0c5a9cdaead9946495bf
Till Klocke <till.klocke@grandcentrix.net>
2023-11-23 10:32:29 +0100

    Moved lock timeout entity to grandcentrix key, to signal that this is not a spring property and therefore does not modify native spring behavior

--- libs/base-entity/src/main/kotlin/net/grandcentrix/component/base/config/DataSourceProperties.kt
+++ libs/base-entity/src/main/kotlin/net/grandcentrix/component/base/config/DataSourceProperties.kt
@@ -2,7 +2,7 @@ package net.grandcentrix.component.base.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties("spring.datasource")
+@ConfigurationProperties("grandcentrix.datasource")
 data class DataSourceProperties(
     val lockTimeoutMs: Long = 3_000
 )

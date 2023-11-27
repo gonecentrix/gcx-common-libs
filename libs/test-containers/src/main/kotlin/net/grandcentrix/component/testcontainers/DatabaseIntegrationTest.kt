889f04e704b5443bc8183f7f78831cabae5a0065
Till Klocke <till.klocke@grandcentrix.net>
2023-11-27 17:06:45 +0100

    now creating the postgres container as bean and using @ServiceConnection to inject datasource details into spring

--- libs/test-containers/src/main/kotlin/net/grandcentrix/component/testcontainers/DatabaseIntegrationTest.kt
+++ libs/test-containers/src/main/kotlin/net/grandcentrix/component/testcontainers/DatabaseIntegrationTest.kt
@@ -1,53 +1,29 @@
 package net.grandcentrix.component.testcontainers
 
-import org.springframework.context.ApplicationContextInitializer
-import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.core.env.PropertySource
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
 import org.testcontainers.containers.PostgreSQLContainer
-import java.util.function.Supplier
 
 @ActiveProfiles("test")
-@ContextConfiguration(initializers = [DatabaseIntegrationTest.DatabasePropertyInitializer::class])
+@ComponentScan("net.grandcentrix.component.testcontainers")
+@Target(AnnotationTarget.CLASS)
 annotation class DatabaseIntegrationTest {
-    companion object {
-        private const val postgresVersion = "14-apline"
 
-        @JvmStatic
-        val postgreSqlContainer =
-            PostgreSQLContainer<Nothing>(
-                "${PostgreSQLContainer.IMAGE}:$postgresVersion"
-            ).apply {
-                start()
-            }
-    }
+    @Configuration
+    @Profile("test")
+    class DatabaseTestConfiguration {
+        private val postgresVersion = "14-alpine"
 
-    class DatabasePropertySource : PropertySource<MutableMap<String, Supplier<Any>>>(
-        "testContainerDatabaseProperties",
-        HashMap()
-    ) {
-        override fun getProperty(name: String): Any? {
-            return source[name]?.get()
-        }
-
-        fun add(name: String, value: Supplier<Any>) {
-            source[name] = value
-        }
-    }
-
-    class DatabasePropertyInitializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
-
-        private val springDataSourceProperty = "spring.datasource"
-
-        override fun initialize(applicationContext: ConfigurableApplicationContext) {
-            applicationContext.environment.propertySources.addFirst(
-                DatabasePropertySource().apply {
-                    add("$springDataSourceProperty.url", postgreSqlContainer::getJdbcUrl)
-                    add("$springDataSourceProperty.username", postgreSqlContainer::getUsername)
-                    add("$springDataSourceProperty.password", postgreSqlContainer::getPassword)
-                }
-            )
+        @Bean
+        @ServiceConnection
+        fun postgresContainer() = PostgreSQLContainer<Nothing>(
+            "${PostgreSQLContainer.IMAGE}:$postgresVersion"
+        ).apply {
+            start()
         }
     }
 }

a839551aaf5ed6f41891120daab46be5576ebe00
Till Klocke <till.klocke@grandcentrix.net>
2023-12-14 15:45:20 +0100

    Added some documentation to the new annotations

--- libs/test-containers/src/main/kotlin/net/grandcentrix/component/testcontainers/BaseIntegrationTest.kt
+++ libs/test-containers/src/main/kotlin/net/grandcentrix/component/testcontainers/BaseIntegrationTest.kt
@@ -9,12 +9,24 @@ import org.springframework.test.context.ActiveProfiles
 @Target(AnnotationTarget.CLASS)
 annotation class IntegrationTest
 
+/**
+ * Annotate test classes with this annotation to create JPA slice tests with a reusable test
+ * container setup. Please note that this annotation only provides the test container and JPA
+ * slice test setup, any project specific configuration like which JPA repositories needs to be
+ * with additional annotations/configuration classes.
+ */
 @Target(AnnotationTarget.CLASS)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DatabaseIntegrationTest
 annotation class DataJpaIntegrationTest
 
+/**
+ * Annotate test classes with this annotation to create full spring boot tests with test
+ * container support. Please note that this annotation only provides the test container and JPA
+ * slice test setup, any project specific configuration like which JPA repositories needs to be
+ * with additional annotations/configuration classes.
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DatabaseIntegrationTest

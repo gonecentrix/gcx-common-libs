a266b0e121b699aa433e90333c91dbf1a48c92c3
Till Klocke <till.klocke@grandcentrix.net>
2024-10-22 09:17:14 +0200

    We do not need to add the spring bom platform here again, since this is already added via our plugins

--- libs/tls-helper/build.gradle.kts
+++ libs/tls-helper/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 
 dependencies {
     implementation(platform("net.grandcentrix.component:gradle-platform"))
-    implementation(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
     implementation("org.bouncycastle:bcpkix-jdk18on")
 }
 
